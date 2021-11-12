b6045cc9cef495a1771835c5a4d8d32644453a62
Raviahar228jsjsiaj <94152823+Raviahar228jsjsiaj@users.noreply.github.com>
2021-11-12 11:31:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,6 @@ module.exports = {
     apiUrl: "",
     // API token from your personal cabinet
     token: ""
-}
+}apiUrl: &quot;https://eu14.chat-api.com/instance12345/&quot;,
+
+token: &quot;xb1s668fphzmr9m&quot;
